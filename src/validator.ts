1f76d0c39c4062af5b966d5f377d573be9384648
David Xie <hk997@live.com>
2024-12-05 00:05:10 +0800

    feat: change validator properties to symbol properties
    BREAKING CHANGE: promise and result have been removed from the public API

--- src/validator.ts
+++ src/validator.ts
@@ -1,4 +1,5 @@
 import { AbortablePromise } from "./abortable-promise"
+import { $fixtures, $plugins, $promise, $result } from "./symbols"
 
 export interface ValidationFixture {
   name: string
@@ -55,69 +56,69 @@ export interface Validator {
 }
 
 export class Validator extends EventTarget {
-  readonly fixtures: ValidationFixture[]
-  readonly plugins: ValidationPlugin[]
-  result: ValidationResult | null
-  promise: AbortablePromise<ValidationResult | void> | null
+  readonly [$fixtures]: ValidationFixture[]
+  readonly [$plugins]: ValidationPlugin[]
+  [$promise]: AbortablePromise<ValidationResult | void> | null
+  [$result]: ValidationResult | null
 
   constructor(fixtures: ValidationFixture[] = [], plugins: ValidationPlugin[] = []) {
     super()
-    this.fixtures = fixtures
-    this.plugins = plugins
-    this.result = null
-    this.promise = null
+    this[$fixtures] = fixtures
+    this[$plugins] = plugins
+    this[$promise] = null
+    this[$result] = null
   }
 
   addFixture(fixture: ValidationFixture): void {
-    this.fixtures.push(fixture)
+    this[$fixtures].push(fixture)
   }
 
   findFixture(name: string | number): ValidationFixture | undefined {
     if (typeof name === "string") {
-      return this.fixtures.find(fixture => fixture.name === name)
+      return this[$fixtures].find(fixture => fixture.name === name)
     }
     else if (typeof name === "number") {
-      return this.fixtures[name]
+      return this[$fixtures][name]
     }
   }
 
   removeFixture(fixture: ValidationFixture | string | number): void {
     let index = -1
     if (typeof fixture === "string") {
-      index = this.fixtures.findIndex(f => f.name === fixture)
+      index = this[$fixtures].findIndex(f => f.name === fixture)
     }
     else if (typeof fixture === "number") {
-      if (fixture >= 0 && fixture < this.fixtures.length) {
+      if (fixture >= 0 && fixture < this[$fixtures].length) {
         index = fixture
       }
     }
     else {
-      index = this.fixtures.findIndex(f => f === fixture)
+      index = this[$fixtures].findIndex(f => f === fixture)
     }
     if (index > -1) {
-      this.fixtures.splice(index, 1)
+      this[$fixtures].splice(index, 1)
     }
   }
 
   addPlugin(plugin: ValidationPlugin): void {
-    this.plugins.push(plugin)
+    this[$plugins].push(plugin)
   }
 
   removePlugin(plugin: ValidationPlugin): void {
-    const index = this.plugins.indexOf(plugin)
+    const index = this[$plugins].indexOf(plugin)
     if (index > -1) {
-      this.plugins.splice(index, 1)
+      this[$plugins].splice(index, 1)
     }
   }
 
   dispatchResult(result: ValidationResult | null): void {
-    this.result = result
+    this[$result] = result
     this.dispatchEvent(new CustomEvent("validation", { detail: result }))
   }
 
   abort(reason?: string): void {
-    if (this.promise) {
-      this.promise.abort(reason)
+    if (this[$promise]) {
+      this[$promise].abort(reason)
     }
   }
 
@@ -128,13 +129,13 @@ export class Validator extends EventTarget {
 
   async validate(trigger?: string): Promise<ValidationResult | null> {
     this.abort(`${trigger} revalidation`)
-    let result = this.result
-    for (const plugin of this.plugins) {
+    let result = this[$result]
+    for (const plugin of this[$plugins]) {
       try {
         const controller = new AbortController()
         const promise = plugin(this, trigger, result, controller.signal)
-        this.promise = AbortablePromise.resolve(promise, controller)
-        result = (await this.promise) || result
+        this[$promise] = AbortablePromise.resolve(promise, controller)
+        result = (await this[$promise]) || result
       }
       catch (error) {
         if (error instanceof AbortError) {
@@ -146,7 +147,7 @@ export class Validator extends EventTarget {
         return { state: "unknown", message: JSON.stringify(error) }
       }
       finally {
-        this.promise = null
+        this[$promise] = null
       }
     }
     this.dispatchResult(result)
