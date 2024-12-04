7aebc429318c9550e366cf601f8924f1d5c55ba6
David Xie <hk997@live.com>
2024-12-04 23:37:03 +0800

    feat: add symbols for internal usage

---
+++ src/symbols.ts
@@ -0,0 +1,5 @@
+export const $controller = Symbol("controller")
+export const $fixtures = Symbol("fixtures")
+export const $plugins = Symbol("plugins")
+export const $promise = Symbol("promise")
+export const $result = Symbol("result")
