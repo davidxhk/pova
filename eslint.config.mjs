5be7ac90e23bdd14e0d1582a6109bc3ac54bd820
David Xie <hk997@live.com>
2024-12-29 02:08:08 +0800

    chore: update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,20 +2,19 @@
 import antfu from "@antfu/eslint-config"
 import globals from "globals"
 
-export default antfu(
-  {
-    type: "lib",
-    typescript: true,
-    languageOptions: {
-      globals: globals.browser,
-    },
-    stylistic: {
-      indent: 2,
-      quotes: "double",
-    },
-    rules: {
-      "ts/method-signature-style": "off",
-      "ts/no-unsafe-declaration-merging": "off",
-    },
+export default antfu({
+  type: "lib",
+  typescript: true,
+  languageOptions: {
+    globals: globals.browser,
   },
-)
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+  },
+  rules: {
+    "ts/method-signature-style": "off",
+    "ts/no-unsafe-declaration-merging": "off",
+    "valid-typeof": "off",
+  },
+})
